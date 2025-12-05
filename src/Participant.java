9064c3fe96d197e7ed65b17836df0e8818b6ab6e
DinithYM <dinithyasi@gmail.com>
2025-12-05 16:12:55 +0530

    Creating participant- setters and getters

--- src/Participant.java
+++ src/Participant.java
@@ -11,3 +11,50 @@ public class Participant extends Person {
         super(id, name);
     }
 
+
+    public String getPreferredGame() {
+        return preferredGame;
+    }
+
+    public void setPreferredGame(String preferredGame) {
+        this.preferredGame = preferredGame;
+    }
+
+    public String getPreferredRole() {
+        return preferredRole;
+    }
+
+    public void setPreferredRole(String preferredRole) {
+        this.preferredRole = preferredRole;
+    }
+
+    public int getSkillRating() {
+        return skillRating;
+    }
+
+    public void setSkillRating(int skillRating) {
+        this.skillRating = skillRating;
+    }
+
+    public int getPersonalityScoreRaw() {
+        return personalityScoreRaw;
+    }
+
+    public int getPersonalityScoreScaled() {
+        return personalityScoreScaled;
+    }
+
+    public void setPersonalityScore(int rawScore) {
+        this.personalityScoreRaw = rawScore;
+        this.personalityScoreScaled = rawScore * 4;
+    }
+
+    public String getPersonalityType() {
+        return personalityType;
+    }
+
+    public void setPersonalityType(String personalityType) {
+        this.personalityType = personalityType;
+    }
+
+
