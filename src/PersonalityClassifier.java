0297779d4123c6e4e30264844f2ba9613f89f543
DinithYM <dinithyasi@gmail.com>
2025-12-05 16:50:40 +0530

    Creating class PersonalityClassifier - implement classification algorithm

--- src/PersonalityClassifier.java
+++ src/PersonalityClassifier.java
@@ -2,3 +2,17 @@ public class PersonalityClassifier {
 
     public void classify(Participant participant) {
         if (participant == null) return;
+
+        int scaled = participant.getPersonalityScoreScaled();
+        String type = "UNKNOWN";
+
+        if (scaled >= 90 && scaled <= 100) {
+            type = "LEADER";
+        } else if (scaled >= 70 && scaled <= 89) {
+            type = "BALANCED";
+        } else if (scaled >= 50 && scaled <= 69) {
+            type = "THINKER";
+        }
+
+        participant.setPersonalityType(type);
+    }
