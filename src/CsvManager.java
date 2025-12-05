883a9c51ba85663f5191e44e3057927593bd612b
DinithYM <dinithyasi@gmail.com>
2025-12-05 16:52:59 +0530

    Creating class csvmanager

---
+++ src/CsvManager.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CsvManager {
+
+
+    private static final String[] EXPECTED_HEADERS = {
+            "ID",
+            "Name",
+            "Email",
+            "PreferredGame",
+            "SkillLevel",
+            "PreferredRole",
+            "PersonalityScore",
+            "PersonalityType"
+    };
+
